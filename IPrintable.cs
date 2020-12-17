cdde3913041bd6e131627d89c5350c7cd3d3b4c2
Iryna <ortofosfatnyi@gmail.com>
2020-12-17 13:18:20 +0200

    Added interface IPrintable

---
+++ IPrintable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12
+{
+    public interface IPrintable
+    {
+        string GetStringRepresentation();
+        void Print();
+        void Println();
+    }
+}
