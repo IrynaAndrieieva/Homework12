3fa8fc184046b95b4b5c8782497409c91bf631d4
Iryna <ortofosfatnyi@gmail.com>
2020-12-17 13:19:15 +0200

    Added interface ICloneable

---
+++ ICloneable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12
+{
+    public interface ICloneableAs<T> : ICloneable
+    {
+        T CloneAs();
+    }
+}
