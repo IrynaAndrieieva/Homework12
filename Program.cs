642cd6e8321348dcf5d733f5e82112c468544cd7
Iryna <ortofosfatnyi@gmail.com>
2020-12-17 18:49:37 +0200

    Trying to test

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace Homework12
     {
         static void Main(string[] args)
         {
+            List lis = new List(3);
+
+            Console.WriteLine(lis.Print("Test 1:"));
+
+            lis.Add(1);
+
+            Console.WriteLine(lis.Print("Test 2:"));
+
+            lis.Add(2);
+            lis.Add(3);
+            Console.WriteLine(lis.Print("Test 3:"));
+
+            Random r = new Random();
+
+            for (int i = 0; i < 30; i++)
+            {
+                lis.Add(r.Next(100));
+            }
+
+            Console.WriteLine(lis.Print("Test 4:"));
+
+            lis.Add(11235813);
+
+            Console.WriteLine(lis.Print());
+
+            Console.ReadKey();
         }
     }
 }
